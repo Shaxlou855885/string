ec997b7ce4c83b68515fa3b52ab5d1115faff0b8
Shaxlou855885 <shaxloubaydullaeva4@gmail.com>
2024-12-09 17:46:16 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+let userName = prompt('Ismingizni kiriting')
+let id = prompt ('space idingizni kiriting')
+let password = prompt ('space passwordingizni kiriting')
+
+const obj = {
+    name:name,
+    id:id,
+    pass:password
+}
+console.log(obj)
